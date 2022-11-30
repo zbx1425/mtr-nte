1bd570be8a384c6fabe5aa547a13c6282fe4e205
zbx1425 <zbx1425@outlook.com>
2022-11-30 10:15:23 +0800

    Revert ModifyArgs to Redirect

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/WindowMixin.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/WindowMixin.java
@@ -4,19 +4,20 @@ import com.mojang.blaze3d.platform.Window;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(Window.class)
 public class WindowMixin {
 
-    @ModifyArgs(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V"), remap = false)
-    private void ctor(Args args) {
-        switch (args.<Integer>get(0)) {
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V"), remap = false)
+    private void ctor(int hint, int value) {
+        switch (hint) {
             case GLFW.GLFW_CONTEXT_VERSION_MAJOR:
             case GLFW.GLFW_CONTEXT_VERSION_MINOR:
-                args.set(1, Math.max(args.<Integer>get(1), 3));
+                GLFW.glfwWindowHint(hint, Math.max(value, 3));
+                break;
+            default:
+                GLFW.glfwWindowHint(hint, value);
                 break;
         }
     }
