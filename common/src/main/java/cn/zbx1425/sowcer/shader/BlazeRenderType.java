64768c15ca660d6c36a96064694ec975a71be06e
zbx1425 <zbx1425@outlook.com>
2023-05-08 13:59:11 +0800

    Change BlazeRenderType use vanilla CompositeState

--- common/src/main/java/cn/zbx1425/sowcer/shader/BlazeRenderType.java
+++ common/src/main/java/cn/zbx1425/sowcer/shader/BlazeRenderType.java
@@ -3,41 +3,32 @@ package cn.zbx1425.sowcer.shader;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import net.minecraft.Util;
-import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-import static net.minecraft.client.renderer.RenderStateShard.*;
-
 public class BlazeRenderType {
 
-    private static final Function<ResourceLocation, RenderType> ENTITY_CUTOUT = Util.memoize(resourceLocation -> {
-        RenderType.CompositeState compositeState = RenderType.CompositeState.builder()
-                .setShaderState(RENDERTYPE_ENTITY_CUTOUT_SHADER)
-                .setTextureState(new RenderStateShard.TextureStateShard((ResourceLocation)resourceLocation, false, false))
-                .setTransparencyState(NO_TRANSPARENCY).setLightmapState(LIGHTMAP).setOverlayState(OVERLAY)
-                .createCompositeState(true);
-        return RenderType.create("entity_cutout", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.TRIANGLES, 256, true, false, compositeState);
-    });
-    private static final Function<ResourceLocation, RenderType> ENTITY_TRANSLUCENT_CULL = Util.memoize(resourceLocation -> {
-        RenderType.CompositeState compositeState = RenderType.CompositeState.builder()
-                .setShaderState(RENDERTYPE_ENTITY_TRANSLUCENT_CULL_SHADER)
-                .setTextureState(new RenderStateShard.TextureStateShard((ResourceLocation)resourceLocation, false, false))
-                .setTransparencyState(TRANSLUCENT_TRANSPARENCY).setLightmapState(LIGHTMAP).setOverlayState(OVERLAY)
-                .createCompositeState(true);
-        return RenderType.create("entity_translucent_cull", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.TRIANGLES, 256, true, true, compositeState);
-    });
-    private static final BiFunction<ResourceLocation, Boolean, RenderType> BEACON_BEAM = Util.memoize((resourceLocation, translucent) -> {
-        RenderType.CompositeState compositeState = RenderType.CompositeState.builder()
-                .setShaderState(RENDERTYPE_BEACON_BEAM_SHADER)
-                .setTextureState(new RenderStateShard.TextureStateShard((ResourceLocation)resourceLocation, false, false))
-                .setTransparencyState(translucent ? TRANSLUCENT_TRANSPARENCY : NO_TRANSPARENCY).setWriteMaskState(translucent ? COLOR_WRITE : COLOR_DEPTH_WRITE)
-                .createCompositeState(false);
-        return RenderType.create("beacon_beam", DefaultVertexFormat.BLOCK, VertexFormat.Mode.TRIANGLES, 256, false, true, compositeState);
-    });
+    private static final Function<ResourceLocation, RenderType> ENTITY_CUTOUT = Util.memoize(resourceLocation ->
+            RenderType.create(
+                "entity_cutout", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.TRIANGLES,
+                256, true, false,
+                ((RenderType.CompositeRenderType)RenderType.entityCutout(resourceLocation)).state()
+            ));
+    private static final Function<ResourceLocation, RenderType> ENTITY_TRANSLUCENT_CULL = Util.memoize(resourceLocation ->
+            RenderType.create(
+                    "entity_translucent_cull", DefaultVertexFormat.NEW_ENTITY, VertexFormat.Mode.TRIANGLES,
+                    256, true, true,
+                    ((RenderType.CompositeRenderType)RenderType.entityTranslucentCull(resourceLocation)).state()
+            ));
+    private static final BiFunction<ResourceLocation, Boolean, RenderType> BEACON_BEAM = Util.memoize((resourceLocation, translucent) ->
+            RenderType.create(
+                    "beacon_beam", DefaultVertexFormat.BLOCK, VertexFormat.Mode.TRIANGLES,
+                    256, false, true,
+                    ((RenderType.CompositeRenderType)RenderType.beaconBeam(resourceLocation, translucent)).state()
+            ));
 
     public static RenderType entityCutout(ResourceLocation resourceLocation) {
         return ENTITY_CUTOUT.apply(resourceLocation);
