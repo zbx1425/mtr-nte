00cf1f8d46ff7bb96140f7ede00ff6900638732d
zbx1425 <zbx1425@outlook.com>
2023-08-14 18:18:19 +0800

    Fix TimingUtil

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TimingUtil.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/TimingUtil.java
@@ -1,32 +1,18 @@
 package cn.zbx1425.mtrsteamloco.render.scripting.util;
 
+import cn.zbx1425.mtrsteamloco.render.RenderUtil;
 import cn.zbx1425.mtrsteamloco.render.scripting.ScriptHolder;
-import mtr.MTRClient;
-import net.minecraft.client.Minecraft;
 
-@SuppressWarnings("unused FieldCanBeLocal")
+@SuppressWarnings("unused")
 public class TimingUtil {
-
-    private static double timeElapsed;
-    private static double frameDelta;
-
-    public static void tick() {
-        final float lastFrameDuration = MTRClient.getLastFrameDuration();
-        if (!Minecraft.getInstance().isPaused()) {
-            timeElapsed += lastFrameDuration / 20;
-            frameDelta = lastFrameDuration / 20;
-        } else {
-            frameDelta = 0;
-        }
-    }
 
     private static double timeElapsedForScript = 0;
     private static double frameDeltaForScript = 0;
 
     public static void prepareForScript(ScriptHolder scriptHolder) {
-        timeElapsedForScript = timeElapsed;
-        frameDeltaForScript = timeElapsed - scriptHolder.lastExecuteTime;
-        scriptHolder.lastExecuteTime = timeElapsed;
+        timeElapsedForScript = RenderUtil.runningSeconds;
+        frameDeltaForScript = timeElapsedForScript - scriptHolder.lastExecuteTime;
+        scriptHolder.lastExecuteTime = timeElapsedForScript;
     }
 
     public static double elapsed() {
