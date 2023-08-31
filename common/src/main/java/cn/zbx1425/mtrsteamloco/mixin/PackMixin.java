5f626be3a36875466aef29997dbf3da8a48bca65
zbx1425 <zbx1425@outlook.com>
2023-08-31 22:07:13 +0800

    Try fixing 1.19.2- build

--- common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/PackMixin.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/mixin/PackMixin.java
@@ -1,11 +1,13 @@
 package cn.zbx1425.mtrsteamloco.mixin;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.PackResources;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackCompatibility;
+#if MC_VERSION >= "11903"
 import net.minecraft.server.packs.resources.IoSupplier;
+import net.minecraft.server.packs.PackResources;
+#endif
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,21 +17,36 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.InputStream;
+import java.util.function.Supplier;
 
 @Mixin(Pack.class)
 public class PackMixin {
 
+#if MC_VERSION >= "11903"
     @Shadow @Final private Pack.ResourcesSupplier resources;
+#else
+    @Shadow @Final private Supplier<PackResources> supplier;
+#endif
     @Shadow @Final private String id;
     @Unique private Boolean isMTRPack = null;
 
     @Inject(method = "getCompatibility", at = @At("HEAD"), cancellable = true)
     private void getCompatibility(CallbackInfoReturnable<PackCompatibility> cir) {
         if (isMTRPack == null) {
+#if MC_VERSION >= "11903"
             try (PackResources packResources = resources.open(id)) {
-                IoSupplier<InputStream> ioSupplier = packResources.getResource(PackType.CLIENT_RESOURCES,
-                        new ResourceLocation("mtr", "mtr_custom_resources.json"));
-                isMTRPack = (Boolean)(ioSupplier != null);
+                IoSupplier<InputStream> ioSupplier;
+#else
+            try (PackResources packResources = supplier.get()) {
+                InputStream ioSupplier;
+#endif
+                try {
+                    ioSupplier = packResources.getResource(PackType.CLIENT_RESOURCES,
+                            new ResourceLocation("mtr", "mtr_custom_resources.json"));
+                    isMTRPack = (ioSupplier != null);
+                } catch (Exception ignored) {
+                    isMTRPack = false;
+                }
             }
         }
         if (isMTRPack) cir.setReturnValue(PackCompatibility.COMPATIBLE);
