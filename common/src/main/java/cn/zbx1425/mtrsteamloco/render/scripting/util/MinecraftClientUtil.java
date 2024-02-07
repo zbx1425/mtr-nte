fbdf54eee00b784f255f654a675580414650711d
Jeffreyg1228 <146730663+Jeffreyg1228@users.noreply.github.com>
2024-02-07 22:30:38 +0800

    Add more APIs for MinecraftClient

--- common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/MinecraftClientUtil.java
+++ common/src/main/java/cn/zbx1425/mtrsteamloco/render/scripting/util/MinecraftClientUtil.java
@@ -1,7 +1,10 @@
 package cn.zbx1425.mtrsteamloco.render.scripting.util;
 
 import cn.zbx1425.sowcer.math.Vector3f;
+import com.mojang.text2speech.Narrator;
+import mtr.mappings.Text;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.player.Player;
 
 public class MinecraftClientUtil {
 
@@ -18,5 +21,16 @@ public class MinecraftClientUtil {
     public static int worldDayTime() {
         return Minecraft.getInstance().level != null
                 ? (int) Minecraft.getInstance().level.getDayTime() : 0;
+    }
+
+    public static void narrate(String message) {
+        Narrator.getNarrator().say(message, true);
+    }
+
+    public static void displayMessage(String message, boolean actionBar) {
+        final Player player = Minecraft.getInstance().player;
+        if (player != null) {
+            player.displayClientMessage(Text.literal(message), actionBar);
+        }
     }
 }
